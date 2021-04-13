917e98381ad9cf40250940c3ac95348f2aacf97b
Japan Patel <Japan Patel@DESKTOP-SEA92IK>
2021-04-13 13:11:50 +0530

    Java project starts.

---
+++ JavaProject/src/javaProject/JavaProject.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package javaProject;
+
+/**
+ * @author Japan Patel
+ *
+ */
+public class JavaProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		
+	}
+
+}
